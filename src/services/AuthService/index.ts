6f1e8b962e1b0dc1871f723857d1f14d8c29ce34
Raiyan109 <kraiyan109@gmail.com>
2024-09-28 09:16:10 +0600

    layouts

---
+++ src/services/AuthService/index.ts
@@ -0,0 +1,10 @@
+'use server'
+
+export const registerUser = async () => {
+    try {
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
