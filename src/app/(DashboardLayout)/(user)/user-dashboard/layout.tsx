9043ef0b9077fc4c49bc34e426d102602113abd4
Raiyan109 <kraiyan109@gmail.com>
2024-09-29 21:49:56 +0600

    user dashboard sidebart

--- src/app/(DashboardLayout)/(user)/user-dashboard/layout.tsx
+++ src/app/(DashboardLayout)/(user)/user-dashboard/layout.tsx
@@ -1,9 +1,47 @@
+import UserSidebar, { SidebarItem } from '@/components/user/UserSidebar'
+import { LayoutDashboard } from 'lucide-react'
 import React, { ReactNode } from 'react'
 
 const UserLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>UserLayout
-            {children}
+        <div className="flex items-start justify-start">
+            <header className=" bg-secondary fixed z-10">
+                <UserSidebar>
+                    <SidebarItem
+                        icon={<LayoutDashboard size={20} />}
+                        text='Dashboard'
+                        link='/profile'
+                        alert
+                    />
+                    {/* <SidebarItem
+                        icon={<MdSportsHandball size={20} />}
+                        text='All Facilities'
+                        link='/admin-dashboard/facilities-table'
+                        alert
+                    />
+                    <SidebarItem
+                        icon={<MdCreateNewFolder size={20} />}
+                        text='Create Facility'
+                        link='/admin-dashboard/create-facility'
+                        alert
+                    />
+                    <SidebarItem
+                        icon={<BookCheck size={20} />}
+                        text='Booking Management'
+                        link='/admin-dashboard/all-bookings'
+                        alert
+                    />
+                    <SidebarItem
+                        icon={<ShieldPlus size={20} />}
+                        text='Add an Admin'
+                        link='/admin-dashboard/make-admin'
+                        alert
+                    /> */}
+                </UserSidebar>
+            </header>
+            <main className="py-8 pl-24 lg:px-52 w-full">
+                {children}
+            </main>
         </div>
     )
 }
