d2dd5269575e24c716ad449d40428fb3495c49c7
Raiyan109 <kraiyan109@gmail.com>
2024-10-03 08:08:18 +0600

    first deploy

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -38,6 +38,7 @@ const Login = () => {
                 router.push("/");
             }
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isPending, isSuccess]);
 
 
