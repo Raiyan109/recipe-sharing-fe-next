01a8b8011f1ea12b7502183e25a11581aec94cca
Raiyan109 <kraiyan109@gmail.com>
2024-10-05 20:48:52 +0600

    review form

--- src/components/home/reviews/Reviews.tsx
+++ src/components/home/reviews/Reviews.tsx
@@ -1,44 +1,58 @@
 import { getSingleUser } from "@/services/AuthService"
 import { IReview } from "@/types"
+import { Star } from "lucide-react"
 import Image from "next/image"
+import ReviewForm from "./ReviewForm"
 
 
+const colors = {
+    orange: "#F2C265",
+    grey: "#D8D8D8"
+}
 
 const Reviews = async ({ review }: { review: IReview }) => {
     const singleUser = await getSingleUser(review?.user)
+    const stars = Array(5).fill(0)
 
     return (
         <div>
-            <div className="flex items-start">
-                <div className="flex-shrink-0">
-                    <div className="inline-block relative">
-                        <div className="relative w-16 h-16 rounded-full overflow-hidden">
-                            <Image className="absolute top-0 left-0 w-full h-full bg-cover object-fit object-cover" src={singleUser?.data?.photo} alt="Profile picture" width={30} height={30} />
-                            <div className="absolute top-0 left-0 w-full h-full rounded-full shadow-inner"></div>
+            <div>
+                <h1 className="text-3xl py-5">Reviews</h1>
+                <div className="flex items-start">
+                    <div className="flex-shrink-0">
+                        <div className="inline-block relative">
+                            <div className="relative w-16 h-16 rounded-full overflow-hidden">
+                                <Image className="absolute top-0 left-0 w-full h-full bg-cover object-fit object-cover" src={singleUser?.data?.photo} alt="Profile picture" width={30} height={30} />
+                                <div className="absolute top-0 left-0 w-full h-full rounded-full shadow-inner"></div>
+                            </div>
+                            <svg className="fill-current text-white bg-green-600 rounded-full p-1 absolute bottom-0 right-0 w-6 h-6 -mx-1 -my-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M19 11a7.5 7.5 0 0 1-3.5 5.94L10 20l-5.5-3.06A7.5 7.5 0 0 1 1 11V3c3.38 0 6.5-1.12 9-3 2.5 1.89 5.62 3 9 3v8zm-9 1.08l2.92 2.04-1.03-3.41 2.84-2.15-3.56-.08L10 5.12 8.83 8.48l-3.56.08L8.1 10.7l-1.03 3.4L10 12.09z" />
+                            </svg>
                         </div>
-                        <svg className="fill-current text-white bg-green-600 rounded-full p-1 absolute bottom-0 right-0 w-6 h-6 -mx-1 -my-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path d="M19 11a7.5 7.5 0 0 1-3.5 5.94L10 20l-5.5-3.06A7.5 7.5 0 0 1 1 11V3c3.38 0 6.5-1.12 9-3 2.5 1.89 5.62 3 9 3v8zm-9 1.08l2.92 2.04-1.03-3.41 2.84-2.15-3.56-.08L10 5.12 8.83 8.48l-3.56.08L8.1 10.7l-1.03 3.4L10 12.09z" />
-                        </svg>
                     </div>
-                </div>
-                <div className="ml-6">
-                    <p className="flex items-baseline">
-                        <span className="text-gray-600 font-bold">{singleUser?.data?.name}</span>
-                        <span className="ml-2 text-green-600 text-xs">{singleUser?.data?.email}</span>
-                    </p>
-                    <div className="flex items-center mt-1">
-                        <svg className="w-4 h-4 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
-                        <svg className="w-4 h-4 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
-                        <svg className="w-4 h-4 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
-                        <svg className="w-4 h-4 fill-current text-yellow-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
-                        <svg className="w-4 h-4 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" /></svg>
-                    </div>
+                    <div className="ml-6">
+                        <p className="flex items-baseline">
+                            <span className="text-gray-600 font-bold">{singleUser?.data?.name}</span>
+                            <span className="ml-2 text-green-600 text-xs">{singleUser?.data?.email}</span>
+                        </p>
+                        <div className="flex items-center mt-1">
+                            {stars.map((_, index) => {
+                                return (
+                                    <Star
+                                        key={index}
+                                        size={24}
+                                        fill={(review.rating) > index ? colors.orange : colors.grey}
+                                        strokeWidth={0}
+                                    />
+                                )
+                            })}
+                        </div>
 
-                    <div className="mt-3">
-                        <span className="font-bold">Comment</span>
-                        <p className="mt-1">{review.comment}</p>
-                    </div>
-                    {/* <div className="flex items-center justify-between mt-4 text-sm text-gray-600 fill-current">
+                        <div className="mt-3">
+                            {/* <span className="font-bold">Comment</span> */}
+                            <p className="mt-1">{review.comment}</p>
+                        </div>
+                        {/* <div className="flex items-center justify-between mt-4 text-sm text-gray-600 fill-current">
                         <button className="flex items-center">
                             <svg className="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5.08 12.16A2.99 2.99 0 0 1 0 10a3 3 0 0 1 5.08-2.16l8.94-4.47a3 3 0 1 1 .9 1.79L5.98 9.63a3.03 3.03 0 0 1 0 .74l8.94 4.47A2.99 2.99 0 0 1 20 17a3 3 0 1 1-5.98-.37l-8.94-4.47z" /></svg>
                             <span className="ml-2">Share</span>
@@ -55,7 +69,12 @@ const Reviews = async ({ review }: { review: IReview }) => {
                             </button>
                         </div>
                     </div> */}
+                    </div>
                 </div>
+            </div>
+
+            <div className="py-10">
+                <ReviewForm />
             </div>
         </div>
     )
