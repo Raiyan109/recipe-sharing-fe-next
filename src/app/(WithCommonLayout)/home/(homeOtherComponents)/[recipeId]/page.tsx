7d1077bf33f970fefc5857e9d1673e6c7b634347
Raiyan109 <kraiyan109@gmail.com>
2024-10-05 20:58:14 +0600

    created new dynamic folder [recipeId] in home route in WithCommonLayout (not in (home) group layout) then copy pasted the code of @recipes [recipeId] and paste in here

---
+++ src/app/(WithCommonLayout)/home/(homeOtherComponents)/[recipeId]/page.tsx
@@ -0,0 +1,21 @@
+import RecipeDetails from "@/components/home/recipeFeed/RecipeDetails";
+import { getSingleRecipe } from "@/services/RecipeService";
+
+
+interface RecipeId {
+    params: {
+        recipeId: string;
+    };
+}
+
+
+const page = async ({ params }: RecipeId) => {
+    const recipe = await getSingleRecipe(params.recipeId)
+    return (
+        <div>
+            <RecipeDetails recipe={recipe.data} />
+        </div>
+    )
+}
+
+export default page
