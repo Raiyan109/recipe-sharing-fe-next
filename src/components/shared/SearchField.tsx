fb0b5dd509b89fa18f4a025f620e3a48a917a76e
Raiyan109 <kraiyan109@gmail.com>
2024-10-08 14:28:14 +0600

    search field link done

--- src/components/shared/SearchField.tsx
+++ src/components/shared/SearchField.tsx
@@ -6,6 +6,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import { IRecipe, IRecipes } from "@/types";
 import { useState } from "react";
+import Link from "next/link";
 
 
 export default function SearchField({ recipes }: { recipes: IRecipes }) {
@@ -46,12 +47,12 @@ export default function SearchField({ recipes }: { recipes: IRecipes }) {
                         <div className="mt-3 space-y-3 absolute bg-white/20 top-12 z-20 backdrop-blur-sm w-96">
                             {filtered?.length > 0 ? (
                                 filtered?.map((recipe) => (
-                                    <div
+                                    <Link href={`/recipe/${recipe?._id}`}
                                         key={recipe._id}
-                                        className="  font-bold cursor-pointer rounded-2xl px-3 py-3 text-lg w-56"
+                                        className="block font-bold cursor-pointer rounded-2xl px-3 py-3 text-lg w-full bg-black/20 hover:bg-black/10"
                                     >
                                         <h1 className="">{recipe.title}</h1>
-                                    </div>
+                                    </Link>
                                 ))
                             ) : (
                                 <div>No results found</div>
