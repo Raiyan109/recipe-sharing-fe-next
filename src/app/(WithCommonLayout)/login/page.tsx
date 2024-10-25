11ab13376327094fbdc8fae6ea660ab593f9673e
Raiyan109 <kraiyan109@gmail.com>
2024-10-25 09:25:42 +0600

    go back button and login image change

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import loginImg from '@/assets/login.jpg'
+import loginImg from '@/assets/recipe-sharing-for-login.png'
 import ReusableForm from '@/components/form/ReusableForm'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import { useRouter, useSearchParams } from 'next/navigation'
@@ -9,6 +9,7 @@ import { useEffect } from 'react'
 import { useUserLogin } from '@/hooks/auth.hook'
 import { useUser } from '@/context/user.provider'
 import Link from 'next/link'
+import GoBack from '@/components/GoBack'
 
 
 const Login = () => {
@@ -47,7 +48,7 @@ const Login = () => {
         <>
             {isPending && <GlassSpinner />}
             <div className='w-full h-screen flex items-start'>
-                <div className='relative w-1/2 h-full hidden lg:flex flex-col '>
+                {/* <div className='relative w-1/2 h-full hidden lg:flex flex-col '>
                     <div className='absolute top-[20%] left-[10%] flex flex-col'>
                         <h1 className='text-3xl text-white font-bold my-4'>Turn your ideas into reality</h1>
                         <p className='text-xl text-white font-normal'>Start for free and get attractive offers from the community</p>
@@ -59,9 +60,27 @@ const Login = () => {
                         alt='login image'
                         className='w-full h-full object-cover'
                     />
+                </div> */}
+                <div
+                    className="hidden flex-col justify-center overflow-hidden bg-cover bg-center lg:flex md:w-1/2 h-full"
+                    style={{
+                        backgroundImage: 'url(https://img.freepik.com/free-vector/gray-neural-network-illustration_53876-78764.jpg?size=626&ext=jpg)'
+                    }}
+                >
+                    <Image
+                        className="-translate-x-[8%] rounded-[36px] shadow-[0_24px_88px_rgba(0,0,0,0.55)]"
+                        height={900}
+                        width={900}
+                        src={loginImg}
+                        alt="Recipe Home page Mockup"
+                    />
                 </div>
 
-                <div className='w-full lg:w-1/2 h-full bg-white flex flex-col p-20 justify-between'>
+
+                <div className='w-full lg:w-1/2 h-full bg-white flex flex-col p-20 justify-between'
+                >
+
+                    <GoBack />
                     <h1 className='text-xl font-semibold'>Culinary Circle</h1>
 
 
