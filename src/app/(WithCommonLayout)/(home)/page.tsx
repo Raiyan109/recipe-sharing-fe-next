5a0db625e3128e5927cb69380293f368ef07492f
Raiyan kabir <raiyankabir@Raiyans-Laptop.netis.cc>
2024-11-07 20:34:20 +0600

    consolelogs removed

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,22 +1,19 @@
-import Recipes from "@/components/home/recipeFeed/Recipes"
-import UsersAvatars from "@/components/home/usersAvatars/UsersAvatars"
-import { getAllUsers, getAnUser } from "@/services/AuthService"
-import { getRecipes } from "@/services/RecipeService"
-
-
+import Recipes from "@/components/home/recipeFeed/Recipes";
+import UsersAvatars from "@/components/home/usersAvatars/UsersAvatars";
+import { getAllUsers, getAnUser } from "@/services/AuthService";
+import { getRecipes } from "@/services/RecipeService";
 
 const HomePage = async () => {
-  const users = await getAllUsers()
-  const recipes = await getRecipes()
-  const user = await getAnUser()
-  console.log(user, 'from home page');
-  
+  const users = await getAllUsers();
+  const recipes = await getRecipes();
+  const user = await getAnUser();
+
   return (
     <div>
       {user && <UsersAvatars users={users?.data} />}
       <Recipes recipes={recipes} user={user} />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/components/home/follow/FollowButton.tsx
+++ src/components/home/follow/FollowButton.tsx
@@ -1,56 +1,61 @@
-'use client'
+"use client";
 
 import { useFollow, useUnfollow } from "@/hooks/follow.hook";
 import { IUser } from "@/types";
 import { Check } from "lucide-react";
 
-
 type IProps = {
-    success: boolean,
-    statusCode: number,
-    message: string,
-    data: IUser[]
-}
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: IUser[];
+};
 
-const FollowButton = ({ userId, followees }: { userId: string, followees: IProps }) => {
-    console.log(userId, 'user id from followbutton');
-    console.log(followees, 'followees from followbutton');
+const FollowButton = ({
+  userId,
+  followees,
+}: {
+  userId: string;
+  followees: IProps;
+}) => {
+  // const { data } = useGetFollowees(userId);
+  // console.log(data);
 
-    // const { data } = useGetFollowees(userId);
-    // console.log(data);
+  const followeesId = followees?.data?.map((f) => f?._id);
+  const isFollowing = followeesId?.includes(userId);
 
-    const followeesId = followees?.data?.map((f) => f?._id)
-    const isFollowing = followeesId?.includes(userId);
-    console.log(isFollowing);
+  const { mutate: handleFollow } = useFollow();
+  const { mutate: handleUnFollow } = useUnfollow();
 
+  const handleFollowUser = () => {
+    handleFollow({ followeeId: userId });
+  };
 
-    const { mutate: handleFollow } = useFollow();
-    const { mutate: handleUnFollow } = useUnfollow();
+  const handleUnFollowUser = () => {
+    handleUnFollow({ followeeId: userId });
+  };
 
+  return (
+    <div>
+      <button
+        className={`px-3 py-1 text-xs font-medium ${
+          isFollowing
+            ? "bg-destructive hover:bg-destructive/80"
+            : "bg-primary hover:bg-primary/80"
+        } text-primary-foreground hover:bg-blue-100 rounded-md transition duration-150 ease-in-out `}
+        onClick={isFollowing ? handleUnFollowUser : handleFollowUser}
+      >
+        {isFollowing ? (
+          <div className="flex items-center gap-2">
+            <p>Following</p>
+            <Check size={20} />
+          </div>
+        ) : (
+          "Follow"
+        )}
+      </button>
+    </div>
+  );
+};
 
-    const handleFollowUser = () => {
-        handleFollow({ followeeId: userId });
-    }
-
-    const handleUnFollowUser = () => {
-        handleUnFollow({ followeeId: userId });
-    }
-
-    return (
-        <div>
-            <button
-                className={`px-3 py-1 text-xs font-medium ${isFollowing ? 'bg-destructive hover:bg-destructive/80' : 'bg-primary hover:bg-primary/80'} text-primary-foreground hover:bg-blue-100 rounded-md transition duration-150 ease-in-out `}
-                onClick={isFollowing ? handleUnFollowUser : handleFollowUser}
-            >
-                {isFollowing ? (
-                    <div className="flex items-center gap-2">
-                        <p>Following</p>
-                        <Check size={20} />
-                    </div>
-                ) : 'Follow'}
-            </button>
-        </div>
-    )
-}
-
-export default FollowButton
+export default FollowButton;
