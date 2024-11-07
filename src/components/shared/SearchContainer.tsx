c627e39ffb41958930415e27cead3ae7ad9b3080
Raiyan kabir <raiyankabir@Raiyans-Laptop.netis.cc>
2024-11-07 20:11:42 +0600

    search input in header improved

--- src/components/shared/SearchContainer.tsx
+++ src/components/shared/SearchContainer.tsx
@@ -1,23 +1,30 @@
-'use client'
+"use client";
 
 import { IRecipe, IRecipes, IUser } from "@/types";
 import Recipe from "../home/recipeFeed/Recipe";
 import BlurredRecipe from "../premium/BlurredRecipe";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import GoBack from "../GoBack";
 
 type IUserProps = {
-    success: boolean;
+  success: boolean;
   statusCode: number;
   message: string;
   data: IUser;
-}
+};
 
-const SearchContainer = ({recipes,user}: {recipes: IRecipes, user: IUserProps}) => {
-    const [recipeData, setRecipeData] = useState()
-    const searchParams = useSearchParams()
+const SearchContainer = ({
+  recipes,
+  user,
+}: {
+  recipes: IRecipes;
+  user: IUserProps;
+}) => {
+  const [recipeData, setRecipeData] = useState<IRecipe[]>([]);
+  const searchParams = useSearchParams();
 
-  // Now get the query 
+  // Now get the query
 
   const searchQuery = searchParams && searchParams.get("q"); // we use `q` to set the query to the browser, it could be anything
 
@@ -26,59 +33,58 @@ const SearchContainer = ({recipes,user}: {recipes: IRecipes, user: IUserProps}) 
       // Filter the data based on search query
       const findRecipe = recipes?.data.filter((recipe) => {
         if (searchQuery) {
-          return recipe?.title?.toLowerCase().includes(searchQuery.toLowerCase()) 
-
-            // user.role.toLowerCase().includes(searchQuery.toLowerCase()) ||
-
-            // user.username.toLowerCase().includes(searchQuery.toLowerCase()) ||
-
-            // user.email.toLowerCase().includes(searchQuery.toLowerCase())
-          
+          return recipe?.title
+            ?.toLowerCase()
+            .includes(searchQuery.toLowerCase());
         } else {
           // If no search query, return the original data
           return true;
         }
       });
-
-
-      // Update profileData based on search results
-
-      setRecipeData(findRecipe);
-
+      setRecipeData(findRecipe || []);
     };
 
-
-    // Call handleSearch when searchQuery changes
-
     handleSearch();
-
-  }, [searchQuery]); // Only rerun the effect if searchQuery changes
+  }, [searchQuery, recipes]); // Only rerun the effect if searchQuery changes
 
   return (
-    <div className="max-w-7xl container mx-auto">
-    <div className="p-5 bg-card rounded-md flex items-center justify-center ">
-    <div className="p-5 rounded-md w-full relative">
-                <div className="px-8 space-y-3">
-                    <div className="grid grid-cols-1 gap-7">
-                        {recipes?.data?.map((recipe: IRecipe) => {
-                            // Check if the recipe is free and the user has premium membership
-                            const isFree = recipe.contentAvailability === 'free';
-                            const isPremiumUser = user?.data?.membership === 'premium';
+    <div>
+      <div className="pl-24 pb-5">
+        <GoBack />
+      </div>
+      <div className="pl-24 pb-12">
+        <h1 className="text-2xl">
+          Showing result for <span className="font-bold">{searchQuery}</span>
+        </h1>
+      </div>
+      <div className="max-w-7xl container mx-auto">
+        <div className="p-5 bg-card rounded-md flex items-center justify-center ">
+          <div className="p-5 rounded-md w-full relative">
+            <div className="px-8 space-y-3">
+              <div className="grid grid-cols-1 gap-7">
+                {recipeData?.length > 0 ? (
+                  recipeData.map((recipe: IRecipe) => {
+                    // Check if the recipe is free and the user has premium membership
+                    const isFree = recipe.contentAvailability === "free";
+                    const isPremiumUser = user?.data?.membership === "premium";
 
-                            // Render Recipe or BlurredRecipe based on the conditions
-                            if (isFree || isPremiumUser) {
-                                return <Recipe key={recipe._id} recipe={recipe} />;
-                            } else {
-                                return <BlurredRecipe key={recipe._id} recipe={recipe} />;
-                            }
-                        })}
-                    </div>
-                </div>
+                    // Render Recipe or BlurredRecipe based on the conditions
+                    if (isFree || isPremiumUser) {
+                      return <Recipe key={recipe._id} recipe={recipe} />;
+                    } else {
+                      return <BlurredRecipe key={recipe._id} recipe={recipe} />;
+                    }
+                  })
+                ) : (
+                  <p>No recipes found matching your search.</p>
+                )}
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-</div>
-</div>
-  )
-}
-
-export default SearchContainer
+export default SearchContainer;
--- src/components/shared/SearchInput.tsx
+++ src/components/shared/SearchInput.tsx
@@ -1,72 +1,45 @@
-'use client'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 
 import { useRouter } from "next/navigation";
 import { useState, ChangeEvent } from "react";
 import { Input } from "../ui/input";
 
-
 interface iDefault {
-    defaultValue: string | null
+  defaultValue: string | null;
 }
-
 
 export const SearchInput = ({ defaultValue }: iDefault) => {
-    // initiate the router from next/navigation
-    const router = useRouter()
-    // We need to grab the current search parameters and use it as default value for the search input
-    const [inputValue, setValue] = useState(defaultValue)
+  // initiate the router from next/navigation
+  const router = useRouter();
+  // We need to grab the current search parameters and use it as default value for the search input
+  const [inputValue, setValue] = useState(defaultValue);
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) =>{
-        const inputValue = event.target.value;
-        setValue(inputValue);
-    }
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setValue(inputValue);
+  };
 
+  const handleSearch = () => {
+    if (inputValue) return router.push(`/search?q=${inputValue}`);
+    if (!inputValue) return router.push("/");
+  };
 
+  const handleKeyPress = (event: { key: any }) => {
+    if (event.key === "Enter") return handleSearch();
+  };
 
-    // If the user clicks enter on the keyboard, the input value should be submitted for search 
-
-    // We are now routing the search results to another page but still on the same page
-
-
-    const handleSearch = () => {
-
-        if (inputValue) return router.push(`/search?q=${inputValue}`);
-
-        if (!inputValue) return router.push("/")
-
-    }
-
-
-    const handleKeyPress = (event: { key: any; }) => {
-
-        if (event.key === "Enter") return handleSearch()
-
-    }
-
-
-
-    return (
-
-        <div className="">
-
-            {/* <label htmlFor="inputId">searchIcon</label> */}
-
-
-            <Input type="text"
-
-                id="inputId"
-
-                placeholder="Enter your keywords"
-
-                value={inputValue ?? ""} onChange={handleChange}
-
-                onKeyDown={handleKeyPress}
-
-                className="bg-[transparent] w-full py-3 pl-2 pr-3" />
-
-
-        </div>
-
-    )
-
-}
+  return (
+    <div className="">
+      <Input
+        type="text"
+        id="inputId"
+        placeholder="Enter your keywords"
+        value={inputValue ?? ""}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        className="bg-[transparent] w-full py-3 pl-2 pr-3"
+      />
+    </div>
+  );
+};
