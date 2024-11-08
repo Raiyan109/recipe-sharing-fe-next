c70ebbc5b1f8fd365326e3aef7665ef1c6926a0b
Raiyan109 <kraiyan109@gmail.com>
2024-11-08 15:19:44 +0600

    contact dark color fixed

--- src/app/(WithCommonLayout)/home/(homeOtherComponents)/contact/page.tsx
+++ src/app/(WithCommonLayout)/home/(homeOtherComponents)/contact/page.tsx
@@ -3,35 +3,35 @@
 const ContactPage = () => {
     return (
         <div className="flex justify-center items-center container mx-auto">
-            <div className="bg-gray-100">
+            <div className="bg-card">
                 <div className=" py-12">
                     <div className="max-w-lg mx-auto px-4">
-                        <h2 className="text-3xl font-semibold text-gray-900 mb-4 text-center">
+                        <h2 className="text-3xl font-semibold mb-4 text-center">
                             How can we help you
                         </h2>
 
-                        <form className="bg-white rounded-lg px-6 py-8 shadow-md">
+                        <form className="bg-secondary rounded-lg px-6 py-8 shadow-md">
                             <div className="mb-4">
-                                <label className="block text-gray-700 font-bold mb-2">Name</label>
+                                <label className="block  font-bold mb-2">Name</label>
                                 <input
-                                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    className="appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
                                     id="name" type="text" placeholder="Enter your name" />
                             </div>
                             <div className="mb-4">
-                                <label className="block text-gray-700 font-bold mb-2">Email</label>
+                                <label className="block  font-bold mb-2">Email</label>
                                 <input
-                                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    className="appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
                                     id="email" type="email" placeholder="Enter your email" />
                             </div>
                             <div className="mb-4">
-                                <label className="block text-gray-700 font-bold mb-2">Message</label>
+                                <label className="block  font-bold mb-2">Message</label>
                                 <textarea
-                                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    className="appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
                                     id="message" placeholder="Enter your message"></textarea>
                             </div>
-                            <div className="flex justify-end">
+                            <div className="flex w-full">
                                 <button
-                                    className="bg-primary hover:bg-primary/80 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    className="bg-primary w-full hover:bg-primary/80 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                     type="button">
                                     Send
                                 </button>
